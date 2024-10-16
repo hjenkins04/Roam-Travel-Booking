b969f26565fdddf3f13ff34b15ae8fa6f0126caa
ryanvandrunen <ryanvandrunen@oscvi.com>
2024-10-15 21:03:21 -0400

    landingheader, changed page back to landing page

--- roam-frontend/app/page.tsx
+++ roam-frontend/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import dynamic from "next/dynamic";
+import LandingHeader from "@/components/LandingHeader";
 import Header from "@/components/Header";
 import SearchBox from "@/components/SearchBox";
 import Underline from "@/components/Effects/TextUnderline";
@@ -41,42 +42,43 @@ export default function Home() {
 
   return (
     <div className="relative min-h-screen">
-      <Header headerSize={"tall"} logoColour={"orange"} />
-
       {/* Background SVG */}
-      {/* <div className="absolute inset-0">
+      <div className="absolute inset-0">
         <LandingPageBackground />
-      </div> */}
+      </div>
 
-      {/* <Header openLoginDrawer={openLoginDrawer} openSignupDrawer={openSignupDrawer} />
+      <LandingHeader
+        openLoginDrawer={openLoginDrawer}
+        openSignupDrawer={openSignupDrawer}
+      />
 
       <main className="relative z-10 px-4 py-8 flex flex-col items-center">
-      <div className="grid grid-cols-5 gap-4 items-center"> */}
-
-      {/* Left 2 Columns: Text */}
-      {/* <div className="col-span-2 whitespace-nowrap xl:ml-24">
+        <div className="grid grid-cols-5 gap-4 items-center">
+          {/* Left 2 Columns: Text */}
+          <div className="col-span-2 whitespace-nowrap xl:ml-24">
             <LandingPageText />
           </div>
-        </div> */}
+        </div>
 
-      {/* Search Box (Center Overlap with Background) */}
-      {/* <div className="relative w-full max-w-6xl z-10 -top-14 py-10" style={{ paddingTop: "calc(50vh - 150px)" }}>
+        {/* Search Box (Center Overlap with Background) */}
+        <div
+          className="relative w-full max-w-6xl z-10 -top-14 py-10"
+          style={{ paddingTop: "calc(50vh - 150px)" }}
+        >
           <SearchBox />
-        </div> */}
-
-      {/* </main> */}
+        </div>
+      </main>
 
       {/* Trending Locations Grid */}
-      {/* <TrendingLocationsHomeGrid /> */}
+      <TrendingLocationsHomeGrid />
 
       {/* Footer */}
 
-      {/* <LoginSignupPopout
+      <LoginSignupPopout
         isLoginOpen={isLoginOpen}
         isSignupOpen={isSignupOpen}
         closeDrawer={closeDrawer}
-      />  */}
-      <ProfilePage />
+      />
     </div>
   );
 }
