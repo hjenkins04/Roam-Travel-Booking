0f35c8a7ebfbff6da7789d92d391df696848b89f
xonixdev <xonix.modifications@gmail.com>
2024-10-17 16:31:24 -0400

    Start of AirplaneSeatSelection

---
+++ roam-frontend/app/SeatBookingPage/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React, { useState } from "react";
+import AirplaneSeatSelection from  "@/components/AirplaneSeatSelection";
+
+export default function SeatBookingPage() {
+    return (
+        <div>
+            <AirplaneSeatSelection />
+        </div>
+    )}
