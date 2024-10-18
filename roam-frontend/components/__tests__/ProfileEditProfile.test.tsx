911a49a8545c70129048c2b1e75a1f949d7d78b5
ryanvandrunen <ryanvandrunen@oscvi.com>
2024-10-18 16:24:33 -0400

    EditProfile now a Component, EditProfile tests

---
+++ roam-frontend/components/__tests__/ProfileEditProfile.test.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import EditProfile from "../EditProfile";
+
+/**
+ * Test File: Profile EditProfile Component
+ *
+ * Purpose:
+ * - Ensures the functionality and rendering behavior of the Profile EditProfile component.
+ * - The Profile EditProfile component includes:
+ *      - Form fields for user account information, such as name, email, phone and address.
+ *      - A form sumbmission button that triggers the handleSubmit function.
+ *      - Optional fields for middle name, suffix, and apartment number.
+ *
+ * Test Cases:
+ * 1. Render the required form fields.
+ *    - Expectation: the rqeuired fields (first, last, email, phone, address, province, date of birth, postal code) should be visible.
+ *
+ * 2. Submit the form and call the handleSubmit function.
+ *    - Expectation: when the form is submitted, the handleSubmit function should be called once.
+ *
+ * 3. Render the optional fields.
+ *    - Expectation: the optional fields (middle name, suffix, apt number) should be visible.
+ */
+
+describe("EditProfile Component", () => {
+  const mockHandleSubmit = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("Render the required form fields", () => {
+    render(<EditProfile handleSubmit={mockHandleSubmit} />);
+
+    // Check required fields
+    const placeholders = [
+      "First Name*",
+      "Last Name*",
+      "Email Address*",
+      "Phone Number*",
+      "Street Address*",
+      "Province*",
+      "Postal Code*",
+    ];
+    for (var s of placeholders) {
+      var element = screen.getByPlaceholderText(s);
+      expect(element).toBeInTheDocument();
+      expect(element).toBeRequired();
+    }
+  });
+
+  test("Submit the form and call the handleSubmit function", () => {
+    render(<EditProfile handleSubmit={mockHandleSubmit} />);
+
+    const submitButton = screen.getByTestId("submit-button");
+    fireEvent.click(submitButton);
+
+    expect(mockHandleSubmit).toHaveBeenCalledTimes(1);
+  });
+
+  test("Render the optional form fields", () => {
+    render(<EditProfile handleSubmit={mockHandleSubmit} />);
+
+    const placeholders = ["Middle", "Apt Number", "Suffix"];
+    for (var s of placeholders) {
+      var element = screen.getByPlaceholderText(s);
+      expect(element).toBeInTheDocument();
+      expect(element).not.toBeRequired();
+    }
+  });
+});
