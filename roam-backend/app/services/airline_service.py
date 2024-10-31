ff880768141e41aa7d26f6171224aa2919a564a0
madisonmacneil <121136189+madisonmacneil@users.noreply.github.com>
2024-10-31 18:52:06 -0400

    airline changes....

--- roam-backend/app/services/airline_service.py
+++ roam-backend/app/services/airline_service.py
@@ -3,7 +3,7 @@ from app.models.dto.airline_dto import AirlineDTO
 from app.repositories.airline_repository import AirlineRepository
 from app.models.entities.airline_entity import AirlineEntity
 
-class AirportService:
+class AirlineService:
     @staticmethod
     def create_airline(airline_dto: AirlineDTO) -> AirlineEntity:
         """Create a new airport and add it to the database."""
